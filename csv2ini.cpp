13dae3e19384f317735799c5db40975552ceeb38
Andrew <102364966+Bvtttks@users.noreply.github.com>
2024-12-20 18:37:37 +0300

    Add files via upload

---
+++ csv2ini.cpp
@@ -0,0 +1,69 @@
+#include <filesystem>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+
+using namespace std;
+namespace fs = std::filesystem;
+
+class IniConverter
+{
+public:
+    void FolderToIni(filesystem::path CsvFolderPath) {
+        string IniFolderPath = CsvFolderPath.string() + "_ini";
+        fs::create_directory(IniFolderPath);
+        IniFolderPath += '/';
+        size_t size = CsvFolderPath.string().size() + 1;
+        
+        for (const auto & csv : fs::directory_iterator(CsvFolderPath)) {
+            string str = csv.path();
+            if(str.substr(str.size() - 4, 4) != ".csv")
+                continue;
+            ifstream CsvRead(csv);
+            str = IniFolderPath + str.substr(size, str.size() - size - 4) + ".ini";
+            ofstream IniWrite(str);
+            _convert(CsvRead, IniWrite);
+            IniWrite.close();
+            CsvRead.close();
+        }
+    }
+    
+private:
+    void _convert(ifstream& CsvRead, ofstream& IniWrite) {
+        vector<string> chapters;
+        string str;
+        getline(CsvRead, str);
+        while(!str.empty())
+            chapters.push_back(_parse(str));
+        
+        int counter = 1;
+        while(getline(CsvRead, str)) {
+            IniWrite << "[#" << counter << ']' << endl;
+            ++counter;
+            for (auto& chap : chapters)
+                IniWrite << chap << '=' << _parse(str) << endl;
+        }
+    }
+
+    string _parse(string& str) {
+        string result = str;
+         if (str[0] == '"') {
+             size_t pos = result.find_first_of('"', 1);
+             result = result.substr(0, pos + 1);
+             str = str.substr(pos + 2, str.size());
+             return result;
+         }
+        
+        size_t pos = result.find_first_of(',');
+        
+        result = result.substr(0, pos);
+        if (pos == -1) {
+            str.clear();
+        } else {
+            str = str.substr(pos + 1, str.size());
+        }
+        return result;
+    }
+    
+};
